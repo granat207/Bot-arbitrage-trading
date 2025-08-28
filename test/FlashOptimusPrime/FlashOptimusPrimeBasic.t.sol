1b7017f41eb3b861d859be993f739a64cda77c97
Daniel_eth <danielvigo06@gmail.com>
2025-08-29 00:14:14 +0200

    Added FlashOptimusPrime tests

--- test/FlashOptimusPrime/FlashOptimusPrimeBasic.t.sol
+++ test/FlashOptimusPrime/FlashOptimusPrimeBasic.t.sol
@@ -5,11 +5,19 @@ import {Test, console} from "forge-std/Test.sol";
 
 import {FlashOptimusPrime} from "../../src/FlashOptimusPrime/FlashOptimusPrime.sol"; 
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol"; 
+
 contract FlashOptimusPrimeBasicTest is Test {
 
 address public constant pancakeRouterV3 = 0x1b81D678ffb9C0263b24A97847620C99d213eB14;
 
 address public constant uniswapRouterV3 = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45; 
+
+IERC20 public constant usdt = IERC20(0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9);  
+IERC20 public constant usdc = IERC20(0xaf88d065e77c8cC2239327C5EDb3A432268e5831); 
+IERC20 public constant link = IERC20(0xf97f4df75117a78c1A5a0DBb814Af92458539FB4); 
+
+IERC20 public constant weth = IERC20(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1); 
 
 address public bob = makeAddr("bob"); 
 address public david = makeAddr("david"); 
@@ -17,7 +25,70 @@ address public david = makeAddr("david");
 FlashOptimusPrime public flashOptimusPrime; 
 
 function setUp() public {
-flashOptimusPrime = new FlashOptimusPrime(pancakeRouterV3, uniswapRouterV3); 
+	vm.startPrank(david); 
+	flashOptimusPrime = new FlashOptimusPrime(pancakeRouterV3, uniswapRouterV3); 
+	vm.stopPrank();
+}
+
+//withraw tests
+function test_canWithdraw_unit() public {
+	vm.startPrank(david); 
+	deal(address(usdt), address(flashOptimusPrime), 100e6); 
+	flashOptimusPrime.withdraw(address(usdt), 100e6);
+	assertEq(usdt.balanceOf(address(flashOptimusPrime)), 0); 
+	assertEq(usdt.balanceOf(david), 100e6);
+}
+
+function test_canWithdraw_fuzz(uint256 amount) public {
+	amount = bound(amount, 0, 100_000_000e6);
+	vm.startPrank(david); 
+	deal(address(usdt), address(flashOptimusPrime), amount); 
+	flashOptimusPrime.withdraw(address(usdt), amount);
+	assertEq(usdt.balanceOf(address(flashOptimusPrime)), 0); 
+	assertEq(usdt.balanceOf(david), amount); 
+}
+
+function test_cantWithdraw_NotTheOwner() public {
+	deal(address(usdt), address(flashOptimusPrime), 100e6); 
+	vm.expectRevert(abi.encodeWithSelector(FlashOptimusPrime.NotTheOwner.selector, address(this))); 
+	flashOptimusPrime.withdraw(address(usdt), 100e6);
+}
+
+//approveToken tests
+function test_canApproveToken() public {
+	vm.startPrank(david); 
+	flashOptimusPrime.approveToken(address(usdt), address(uniswapRouterV3), 100e6); 
+	vm.stopPrank(); 
+}
+
+function test_cantApproveToken_notTheOwner() public { 
+	vm.expectRevert(abi.encodeWithSelector(FlashOptimusPrime.NotTheOwner.selector, address(this))); 
+	flashOptimusPrime.approveToken(address(usdt), address(uniswapRouterV3), 100e6); 
+}
+
+//setTradeExecutor tests
+function test_canSetTradeExecutor() public {
+	vm.startPrank(david); 
+	flashOptimusPrime.setTradeExecutor(address(bob)); 
+	assertEq(flashOptimusPrime.tradeExecutor(), bob); 
+}
+
+function test_cantSetTradeExecutor_0Address() public {
+	vm.startPrank(david); 
+	vm.expectRevert( "Can't be a 0 address");
+	flashOptimusPrime.setTradeExecutor(address(0)); 
+}
+
+function test_cantSetTradeExecutor_NotSelf() public {
+	vm.startPrank(david); 
+	vm.expectRevert( "Can't be this address"); 
+	flashOptimusPrime.setTradeExecutor(address(flashOptimusPrime)); 
+}
+
+function test_cantSetTradeExecutor_NotTheOwner() public {
+	vm.expectRevert(abi.encodeWithSelector(FlashOptimusPrime.NotTheOwner.selector, address(this))); 
+	flashOptimusPrime.setTradeExecutor(address(123));
+
 }
 
 
--- test/FlashOptimusPrime/FlashOptimusPrimeTrading.t.sol
+++ test/FlashOptimusPrime/FlashOptimusPrimeTrading.t.sol
@@ -5,19 +5,141 @@ import {Test, console} from "forge-std/Test.sol";
 
 import {FlashOptimusPrime} from "../../src/FlashOptimusPrime/FlashOptimusPrime.sol"; 
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol"; 
+
 contract FlashOptimusPrimeTradingTest is Test {
 
 address public constant pancakeRouterV3 = 0x1b81D678ffb9C0263b24A97847620C99d213eB14;
 
 address public constant uniswapRouterV3 = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45; 
 
+IERC20 public constant usdt = IERC20(0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9);  
+IERC20 public constant usdc = IERC20(0xaf88d065e77c8cC2239327C5EDb3A432268e5831); 
+IERC20 public constant link = IERC20(0xf97f4df75117a78c1A5a0DBb814Af92458539FB4); 
+
+IERC20 public constant weth = IERC20(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1); 
+
+address public constant usdcUsdt100 = 0xbE3aD6a5669Dc0B8b12FeBC03608860C31E2eef6; 
+
 address public bob = makeAddr("bob"); 
 address public david = makeAddr("david"); 
+address public tradeExecutor = makeAddr("tradeExecutor"); 
 
 FlashOptimusPrime public flashOptimusPrime; 
 
 function setUp() public {
-flashOptimusPrime = new FlashOptimusPrime(pancakeRouterV3, uniswapRouterV3); 
+	vm.startPrank(david); 
+	flashOptimusPrime = new FlashOptimusPrime(pancakeRouterV3, uniswapRouterV3); 
+
+	flashOptimusPrime.approveToken(address(usdt), address(uniswapRouterV3), type(uint256).max); 
+	flashOptimusPrime.approveToken(address(weth), address(uniswapRouterV3), type(uint256).max); 
+	flashOptimusPrime.approveToken(address(usdt), address(pancakeRouterV3), type(uint256).max); 
+	flashOptimusPrime.approveToken(address(weth), address(pancakeRouterV3), type(uint256).max); 
+	flashOptimusPrime.setTradeExecutor(tradeExecutor); 
+	vm.stopPrank();
+}
+
+//tradeOnUniswapV3AndPancakeV3 tests
+function test_canTradeOnUniswapV3AndPancakeV3_unit() public {
+	vm.startPrank(tradeExecutor); 
+
+	uint24[] memory poolFees = new uint24[](2); 
+	poolFees[0] = 500; 
+	poolFees[1] = 100; 
+	(bool success, ) = address(flashOptimusPrime).call{value: 0}(
+		abi.encodeWithSelector(FlashOptimusPrime.initFlash.selector, FlashOptimusPrime.CodePath.UniswapV3AndPancakeV3, usdcUsdt100, address(usdt), address(weth), 0, 10_000e6, poolFees));
+	
+	if(success) {
+		assertEq(flashOptimusPrime.isFlashLoaning(), false); 
+		assertEq(flashOptimusPrime.tokenBorrowed(), address(usdt)); 
+		assertEq(flashOptimusPrime.tokenToTrade(), address(weth)); 
+		assertEq(flashOptimusPrime.borrowedByPool(), usdcUsdt100); 
+		assertEq(flashOptimusPrime.amountBorrowed(), 10_000e6); 
+		assertEq(flashOptimusPrime.poolFees(0), poolFees[0]); 
+		assertEq(flashOptimusPrime.poolFees(1), poolFees[1]); 
+		assertEq(flashOptimusPrime.isFeeZero(), false); 
+		assertEq(flashOptimusPrime.initialBalance(), 0); 
+		console.log("Able to profit, USDT balance is ", usdt.balanceOf(address(flashOptimusPrime)));
+	} else {
+		console.log("Unable to profit"); 
+	}
+
+	vm.stopPrank(); 
+}
+
+function test_cantTradeOnUniswapV3AndPancakeV3_notTheTradeExecutor() public {
+	uint24[] memory poolFees = new uint24[](2); 
+	poolFees[0] = 500; 
+	poolFees[1] = 100; 
+	vm.expectRevert(abi.encodeWithSelector(FlashOptimusPrime.NotTheTradeExecutor.selector, address(this))); 
+	address(flashOptimusPrime).call{value: 0}(
+		abi.encodeWithSelector(FlashOptimusPrime.initFlash.selector, FlashOptimusPrime.CodePath.UniswapV3AndPancakeV3, usdcUsdt100, address(usdt), address(weth), 0, 10_000e6, poolFees));
+}
+
+function test_cantTradeOnUniswapV3AndPancakeV3_botAmountAreGt0() public {
+	vm.startPrank(tradeExecutor); 
+
+	uint24[] memory poolFees = new uint24[](2); 
+	poolFees[0] = 500; 
+	poolFees[1] = 100; 
+	vm.expectRevert(abi.encodeWithSelector(FlashOptimusPrime.NotTheTradeExecutor.selector, 100e6, 10_000e6)); 
+	address(flashOptimusPrime).call{value: 0}(
+		abi.encodeWithSelector(FlashOptimusPrime.initFlash.selector, FlashOptimusPrime.CodePath.UniswapV3AndPancakeV3, usdcUsdt100, address(usdt), address(weth), 100e6, 10_000e6, poolFees));
+}
+
+
+//tradeOnPancakeV3AndUniswapV3 tests
+function test_canTradeOnPancakeV3AndUniswapV3_unit() public {
+	vm.startPrank(tradeExecutor); 
+
+	uint24[] memory poolFees = new uint24[](2); 
+	poolFees[0] = 100; 
+	poolFees[1] = 500; 
+	(bool success, ) = address(flashOptimusPrime).call{value: 0}(
+		abi.encodeWithSelector(FlashOptimusPrime.initFlash.selector, FlashOptimusPrime.CodePath.PancakeV3AndUniswapV3, usdcUsdt100, address(usdt), address(weth), 0, 10_000e6, poolFees));
+	
+	if(success) {
+		assertEq(flashOptimusPrime.isFlashLoaning(), false); 
+		assertEq(flashOptimusPrime.tokenBorrowed(), address(usdt)); 
+		assertEq(flashOptimusPrime.tokenToTrade(), address(weth)); 
+		assertEq(flashOptimusPrime.borrowedByPool(), usdcUsdt100); 
+		assertEq(flashOptimusPrime.amountBorrowed(), 10_000e6); 
+		assertEq(flashOptimusPrime.poolFees(0), poolFees[0]); 
+		assertEq(flashOptimusPrime.poolFees(1), poolFees[1]); 
+		assertEq(flashOptimusPrime.isFeeZero(), false); 
+		assertEq(flashOptimusPrime.initialBalance(), 0); 
+		console.log("Able to profit, USDT balance is ", usdt.balanceOf(address(flashOptimusPrime)));
+	} else {
+		console.log("Unable to profit"); 
+	}
+
+	vm.stopPrank(); 
+}
+
+function test_cantTradeOnPancakeV3AndUniswapV3_notTheTradeExecutor() public {
+	uint24[] memory poolFees = new uint24[](2); 
+	poolFees[0] = 100; 
+	poolFees[1] = 500; 
+	vm.expectRevert(abi.encodeWithSelector(FlashOptimusPrime.NotTheTradeExecutor.selector, address(this))); 
+	address(flashOptimusPrime).call{value: 0}(
+		abi.encodeWithSelector(FlashOptimusPrime.initFlash.selector, FlashOptimusPrime.CodePath.PancakeV3AndUniswapV3, usdcUsdt100, address(usdt), address(weth), 0, 10_000e6, poolFees));
+}
+
+function test_cantTradeOnPancakeV3AndUniswapV3_botAmountAreGt0() public {
+	vm.startPrank(tradeExecutor); 
+
+	uint24[] memory poolFees = new uint24[](2); 
+	poolFees[0] = 100; 
+	poolFees[1] = 500; 
+	vm.expectRevert(abi.encodeWithSelector(FlashOptimusPrime.NotTheTradeExecutor.selector, 100e6, 10_000e6)); 
+	address(flashOptimusPrime).call{value: 0}(
+		abi.encodeWithSelector(FlashOptimusPrime.initFlash.selector, FlashOptimusPrime.CodePath.PancakeV3AndUniswapV3, usdcUsdt100, address(usdt), address(weth), 100e6, 10_000e6, poolFees));
+}
+
+//uniswapV3FlashCallback tests 
+function test_cantuniswapV3FlashCallback_notTheFlashLoanPool() public {
+	vm.expectRevert(abi.encodeWithSelector(FlashOptimusPrime.NotTheFlashLoanPool.selector, address(this))); 
+	flashOptimusPrime.uniswapV3FlashCallback(100, 100, "");
 }
 
 
